996275c43e61cc4c2b58a70fd6db8e2f2f5ef6a3
sharefeel <sharefeel@gmail.com>
2022-04-23 16:42:55 +0900

    주석 추가

--- src/main/java/net/youngrok/codility/lesson/arrays/OddOccurrencesInArray.java
+++ src/main/java/net/youngrok/codility/lesson/arrays/OddOccurrencesInArray.java
@@ -1,6 +1,9 @@
 package net.youngrok.codility.lesson.arrays;
 
 /*
+ * 이 문제는 웹에서 본 솔루션에 감탄해서 복사해둔 것이다.
+ * ^= 으로 풀 생각을 하다니.. 아마 나는 아무리 시간을 써도 O(N)에는 못풀었을 것이다.
+ *
  * https://codility.com/demo/results/training6QGBSS-523/
  */
 
